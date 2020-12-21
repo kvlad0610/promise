f1cd4dcbe44ddce825c6cc2761fb2f1c7f4647ca
Vlad <kvlad0610@gmail.com>
2020-12-21 15:35:48 +0200

    promise с Webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.css';
+import timer from './js/timer';
+import promisification1 from './js/promisification-1';
+import colorSwitch from './js/color-switch';
+import promisification2 from './js/promisification-2';
+import promisification3 from './js/promisification-3';
